53a8098dfa741ef71dbd6937a20337830a039be8
Shyam4801 <shyamnambiraja01@gmail.com>
2024-09-23 05:01:16 -0700

    Add new projects

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -34,6 +34,34 @@ export default [
       link: 'https://github.com/Shyam4801/non-myopic_bo',
       description: 'A project developing an efficient rollout acquisition function for Bayesian Optimization, enhancing its performance in complex scenarios.',
    },
+   {
+      title: 'Splitwise Clone',
+      imgUrl: 'assets/splitwiselogo.png',
+      stack: ['React', 'Node', 'Express', 'PostgreSQL'],
+      // link: 'https://github.com/Shyam4801/splitwise-clone',
+      description: 'A clone of the popular expense-sharing app, Splitwise, built with modern web technologies.',
+   },
+   {
+      title: 'Optimized Product Catalog Search with Custom B+ Tree',
+      imgUrl: 'assets/b+.png',
+      stack: ['Java'],
+      link: 'https://github.com/Shyam4801/bplustree',
+      description: 'An implementation of a custom B+ tree for efficient product catalog search, enhancing performance and scalability.',
+   },
+   {
+      title: 'Graph algorithm visualizer',
+      imgUrl: 'assets/graphviz-ezgif.com-video-to-gif-converter.gif',
+      stack: ['TypeScript', 'React','Next.js','Tailwind'],
+      link: 'https://github.com/Shyam4801/graph_algorithm_visualizer',
+      description: 'A graph algorithm visualizer that allows users to visualize and understand graph algorithms.',
+   },
+   {
+      title: 'Two Tower Recommendation System',
+      imgUrl: 'assets/2tower.png',
+      stack: ['Python', 'Pytorch', 'Flask', 'React', 'PostgreSQL'],
+      link: 'https://github.com/Shyam4801/two_tower_rec',
+      description: 'A two tower recommendation system built with Pytorch, trained on the MovieLens dataset.',
+   },
 
    
 
